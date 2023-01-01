e492e4c8ca87ef7802c9c49f38edab65b75e0f1a
ryancook3 <60119458+ryancook3@users.noreply.github.com>
2023-01-01 13:57:38 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode } from "react";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
